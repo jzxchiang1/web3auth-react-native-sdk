aec8178b3ac5de49829509443609eeb83068d226
michaellee8 <ckmichael8@gmail.com>
2021-09-07 11:02:38 +0800

    feat: wip\n don't use

--- ios/OpenLoginReactNativeSdk.swift
+++ ios/OpenLoginReactNativeSdk.swift
@@ -1,8 +1,32 @@
+import OpenLogin
+
 @objc(OpenLoginReactNativeSdk)
 class OpenLoginReactNativeSdk: NSObject {
 
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
+    }
+    
+    @objc(init:withResolver:withRejecter:)
+    func `init`(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        guard
+            let clientId = params["clientId"] as? String,
+            let networkString = params["network"] as? String,
+            let network = Network(rawValue: networkString)
+        else {
+            <#statements#>
+        }
+        resolve(null)
+    }
+    
+    @objc(login:withResolver:withRejecter:)
+    func login(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(null)
+    }
+    
+    @objc(logout:withResolver:withRejecter:)
+    func logout(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(null)
     }
 }
--- ios/OpenloginReactNativeSdk.m
+++ ios/OpenloginReactNativeSdk.m
@@ -6,4 +6,16 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(init: (NSDictionary *)params
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+                  
+RCT_EXTERN_METHOD(login: (NSDictionary *)params
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(logout: (NSDictionary *)params
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
 @end
