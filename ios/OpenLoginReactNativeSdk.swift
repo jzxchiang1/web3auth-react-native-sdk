a1fb22538cd0942a454d8866bd834750a44fabdb
michaellee8 <ckmichael8@gmail.com>
2021-09-07 08:52:06 +0800

    feat: refactor ios

--- ios/OpenLoginReactNativeSdk.swift
+++ ios/OpenLoginReactNativeSdk.swift
@@ -1,5 +1,5 @@
-@objc(OpenloginReactNativeSdk)
-class OpenloginReactNativeSdk: NSObject {
+@objc(OpenLoginReactNativeSdk)
+class OpenLoginReactNativeSdk: NSObject {
 
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
--- ios/OpenloginReactNativeSdk.m
+++ ios/OpenloginReactNativeSdk.m
@@ -1,6 +1,6 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(OpenloginReactNativeSdk, NSObject)
+@interface RCT_EXTERN_MODULE(OpenLoginReactNativeSdk, NSObject)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
